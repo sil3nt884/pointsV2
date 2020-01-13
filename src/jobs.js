dd4fbb9146a6dece28d5cb21afa441c351040d5a
ricky <sil3nt884@hotmail.co.uk>
2020-01-13 16:14:29 +0000

    v2

--- src/jobs.js
+++ src/jobs.js
@@ -6,7 +6,7 @@ let scheduler = require('node-schedule')
 
 
 
-export  default () => {
+module.exports=  () => {
 
    let j = scheduler.scheduleJob({hour: 17, minute: 0, dayOfWeek: 1}, async function(){
       await push.sendPushNotification({ title:'Oyster card return now', text:'Return Rickys Oyster', tag:'Oyster'})
