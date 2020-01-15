def8fc915c1565c8a5542684b3368cefccb1929a
ricky <sil3nt884@hotmail.co.uk>
2020-01-15 17:28:17 +0000

    v2

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, '/var/www/html/')));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
